565f28b90e484f61c489d74db9947ec80fc0ab3d
Ronan Cendrier <rcendrier@yahoo.fr>
2019-01-21 12:19:03 +0100

    Crawler: Add removeTags function

--- src/main/kotlin/crawler/Lexer.kt
+++ src/main/kotlin/crawler/Lexer.kt
@@ -1,9 +1,12 @@
 package crawler
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 class Lexer {
     fun getContent(url: String) {
         val doc = Jsoup.connect("http://en.wikipedia.org/").get()
     }
+
+    fun removeTags(document: Document): String = document.text()
 }
