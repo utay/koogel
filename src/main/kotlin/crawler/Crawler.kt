edef78f78033f7d6741108d3b9f06f2a3bb8bb35
Ronan Cendrier <rcendrier@yahoo.fr>
2019-01-21 18:02:26 +0100

    Crawler: crawl hrefs

--- src/main/kotlin/crawler/Crawler.kt
+++ src/main/kotlin/crawler/Crawler.kt
@@ -1,14 +1,61 @@
 package crawler
 
+import org.apache.log4j.BasicConfigurator
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class Crawler {
 
-    private fun getContent(url: String) = Jsoup.connect(url).get().text()
+    private val LOGGER: Logger = LoggerFactory.getLogger(Crawler::class.java)
+    private val crawledUrls = mutableSetOf<String>()
+
+    private fun getContent(url: String) = Jsoup.connect(url).get()
+
+    private fun crawlHrefs(document: Document, baseUrl: String) {
+        val hrefs: Elements = document.select("a")
+        hrefs.forEach {
+            var href = it.attr("href")
+            href = href.replace(Regex("\\?.*"), "").removeSuffix("/")
+            if (!href.isEmpty() && !href.startsWith("#")) {
+                var prefix = ""
+                // for example "/wiki/help"
+                if (!href.startsWith("http") && !href.startsWith("www")) {
+                    var url = baseUrl
+                    if (baseUrl.startsWith("https")) {
+                        url = url.replaceFirst("https://", "")
+                        prefix = "https://"
+                    } else if (href.startsWith("http")) {
+                        url = url.replaceFirst("http://", "")
+                        prefix = "http://"
+                    }
+                    val domain = url.slice(0 until url.indexOf("/"))
+                    href = "$prefix$domain$href"
+                }
+                if (!crawledUrls.contains(href)) {
+                    crawledUrls.add(href)
+                    crawl(href)
+                }
+            }
+        }
+    }
+
+    private fun crawl(url: String) {
+        LOGGER.info("crawling $url")
+        try {
+            val content = getContent(url)
+            crawlHrefs(content, url)
+            val page = Lexer.lex(content.text(), url)
+        } catch (e: Exception) {
+            LOGGER.error("Error fetching url: $url", e)
+        }
+    }
 
     fun main(args: Array<String>?) {
+        BasicConfigurator.configure()
         val url = "https://en.wikipedia.org/wiki/Paris"
-        val content = getContent(url)
-        val page = Lexer.lex(content, url)
+        crawl(url)
     }
 }
