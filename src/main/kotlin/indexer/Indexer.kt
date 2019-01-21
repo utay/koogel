a2999beb9462cc0d56c9f16fa4a8bcd524acd273
Yannick Utard <yannickutard@gmail.com>
2019-01-21 16:48:26 +0100

    indexer: use static methods

--- src/main/kotlin/indexer/Indexer.kt
+++ src/main/kotlin/indexer/Indexer.kt
@@ -8,34 +8,36 @@ import kotlin.collections.HashMap
 
 class Indexer {
 
-    private val LOGGER: Logger = LoggerFactory.getLogger(Indexer::class.java)
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(Indexer::class.java)
 
-    fun index(page: Page) {
-        LOGGER.info("Indexing {}", page.URL)
-        index.addDocument(getDocument(page))
-    }
-
-    fun getDocument(page: Page): Document {
-         val doc = Document(page.URL, HashMap())
-
-        for (word in page.content) {
-            if (!doc.metadata.containsKey(word)) {
-                val pageIndices = ArrayList<Int>()
-
-                for ((index, word2) in page.content.withIndex()) {
-                    if (word == word2) {
-                        pageIndices.add(index)
-                    }
-                }
-
-                doc.metadata[word] = Metadata(
-                    pageIndices.size / page.content.size.toDouble(),
-                    pageIndices,
-                    page.rawIndices[word]!!
-                )
-            }
+        fun index(page: Page) {
+            LOGGER.info("Indexing {}", page.URL)
+            index.addDocument(getDocument(page))
         }
 
-        return doc
+        fun getDocument(page: Page): Document {
+            val doc = Document(page.URL, HashMap())
+
+            for (word in page.content) {
+                if (!doc.metadata.containsKey(word)) {
+                    val pageIndices = ArrayList<Int>()
+
+                    for ((index, word2) in page.content.withIndex()) {
+                        if (word == word2) {
+                            pageIndices.add(index)
+                        }
+                    }
+
+                    doc.metadata[word] = Metadata(
+                        pageIndices.size / page.content.size.toDouble(),
+                        pageIndices,
+                        page.rawIndices[word]!!
+                    )
+                }
+            }
+
+            return doc
+        }
     }
 }
--- src/main/kotlin/indexer/Main.kt
+++ src/main/kotlin/indexer/Main.kt
@@ -3,20 +3,16 @@ package indexer
 import crawler.Page
 import search.Search
 import java.util.*
-import kotlin.collections.ArrayList
-import kotlin.collections.HashMap
 
 val index = Index()
 
 fun main(args: Array<String>) {
-
-    val indexer = Indexer()
     val page = Page("https://en.wikipedia.org/wiki/Main_Page")
     val page2 = Page("https://en.wikipedia.org/wiki/Tata")
     val page3 = Page("https://en.wikipedia.org/wiki/Jamshedpur")
-    indexer.index(page)
-    indexer.index(page2)
-    indexer.index(page3)
+    Indexer.index(page)
+    Indexer.index(page2)
+    Indexer.index(page3)
     val s = Search()
     s.searchQuery(Arrays.asList("day"))
 }
