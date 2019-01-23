52921aece637aa956ff7559e0236ac7eefa39440
Ronan Cendrier <rcendrier@yahoo.fr>
2019-01-23 17:55:48 +0100

    EventBus: Add subscribe and unsubscribe methods

--- src/main/kotlin/eventbus/Client.kt
+++ src/main/kotlin/eventbus/Client.kt
@@ -4,16 +4,56 @@ import com.google.gson.Gson
 import com.mashape.unirest.http.Unirest
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import spark.Spark
+import spark.kotlin.Http
+import spark.kotlin.RouteHandler
+import spark.kotlin.ignite
 
-class Client(val hostname: String, val port: Int, val eventBusUrl: String) {
+class Client(private val host: String, private val port: Int, private val eventBusUrl: String) {
 
-    private val LOGGER: Logger = LoggerFactory.getLogger(Client::class.java)
+    companion object {
+        private val LOGGER: Logger = LoggerFactory.getLogger(Client::class.java)
+    }
+
+    private val http: Http = ignite()
+
+    init {
+        Spark.port(port)
+    }
+
+    fun addHandler(path: String, routeHandler: RouteHandler.() -> Any) {
+        http.post(path) {
+            routeHandler()
+        }
+    }
 
     fun publish(eventMessage: EventMessage) {
         val serializedMessage = Gson().toJson(eventMessage)
         try {
-            Unirest.post(eventBusUrl).body(serializedMessage).asJson()
+            Unirest.post("$eventBusUrl/event").body(serializedMessage).asJson()
             LOGGER.info("Published to channel ${eventMessage.channel}")
+        } catch (e: Exception) {
+            LOGGER.error(e.message)
+        }
+    }
+
+    fun subscribe(channel: String, callbackUrl: String) {
+        val subscribeMessage = SubscribeMessage(callbackUrl)
+        val eventMessage = EventMessage(channel, subscribeMessage.javaClass.typeName, Gson().toJson(subscribeMessage))
+        try {
+            Unirest.post("$eventBusUrl/subscribe").body(eventMessage).asJson()
+            LOGGER.info("Subscribed to channel ${eventMessage.channel}")
+        } catch (e: Exception) {
+            LOGGER.error(e.message)
+        }
+    }
+
+    fun unsubscribe(channel: String, callbackUrl: String) {
+        val subscribeMessage = SubscribeMessage(callbackUrl)
+        val eventMessage = EventMessage(channel, subscribeMessage.javaClass.typeName, Gson().toJson(subscribeMessage))
+        try {
+            Unirest.post("$eventBusUrl/unsubscribe").body(eventMessage).asJson()
+            LOGGER.info("Unsubscribed to channel ${eventMessage.channel}")
         } catch (e: Exception) {
             LOGGER.error(e.message)
         }
