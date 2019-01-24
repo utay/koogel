c8076c89f0dc33212b6928f9df527693c69dc916
Guillaume Dessain <guillaume.dessain@epita.fr>
2019-01-24 13:48:52 +0100

    Bus: move logger

--- src/main/kotlin/application/CrawlerApp.kt
+++ src/main/kotlin/application/CrawlerApp.kt
@@ -46,8 +46,8 @@ class CrawlerApp(eventBus: EventBusClient) : App(eventBus) {
 
     private fun notifyCrawlerManager() {
         val response = sendMessage(CRAWLER_MANAGER_CHANNEL, REGISTER_CRAWLER, RegisterCrawlerSerializer(uid))
-        LOGGER.info("Crawler Manager not Up yet, retrying in 5 sec...")
         if (response != null && response.nbListener == 0) {
+            LOGGER.info("Crawler Manager not Up yet, retrying in 5 sec...")
             timer.schedule(5000) {
                 notifyCrawlerManager()
             }
